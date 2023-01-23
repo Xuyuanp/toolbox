5a3d0c1437d672e3b6f872e165398b810b2f8f6e
Xuyuan Pang <xuyuanp@gmail.com>
2023-01-23 18:36:08 +0800

    Update context.go
    refactor context

--- context/context.go
+++ context/context.go
@@ -7,25 +7,5 @@ import (
 )
 
 func WithSignals(parent context.Context, signals ...os.Signal) (context.Context, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(parent)
-	done := make(chan struct{})
-	chS := make(chan os.Signal, 1)
-	signal.Notify(chS, signals...)
-	go func() {
-		select {
-		case <-chS:
-		case <-done:
-		case <-parent.Done():
-		}
-		signal.Stop(chS)
-		close(chS)
-		cancel()
-	}()
-	return ctx, func() {
-		select {
-		case <-done:
-		default:
-			close(done)
-		}
-	}
+    return signal.NotifyContext(parent, signals...)
 }
